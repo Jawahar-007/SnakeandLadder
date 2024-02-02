e465cec707c70d04afea8f8431c407e1a8851655
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-02-03 02:46:49 +0530

    UC3 Added Checks for No Play,Snake and ladder

--- src/com/bridgelabz/snakeladder/Game.java
+++ src/com/bridgelabz/snakeladder/Game.java
@@ -8,15 +8,37 @@ public class Game {
         Random r = new Random();
         return r.nextInt(6);
     }
+
+    private static String checksfor(){
+        String[] arr = {"No play","Ladder","Snake"};
+        Random r = new Random();
+        int i = r.nextInt(arr.length);
+        return arr[i];
+    }
     public static void playgame(){
         Scanner scanner = new Scanner(System.in);
-        int position =0;
+        int position =0,diceVal = 0;
         System.out.println("Initial Position = "+position);
         System.out.println("Enter (roll) to roll dice");
-        String option = scanner.nextLine();
-        if(option.equalsIgnoreCase("roll")){
-            int diceVal = rolldice();
+        String ch = scanner.nextLine();
+        if(ch.equalsIgnoreCase("roll")){
+            diceVal = rolldice();
             System.out.println("you rolled out "+diceVal+" times");
         }
+        String Option = checksfor();
+        switch(Option){
+            case "Ladder":
+                position += diceVal;
+                break;
+            case "Snake":
+                position -= diceVal;
+                break;
+            case "No play":
+                position = diceVal;
+                break;
+            default:
+                System.out.println("U got nothing");
+        }
+        System.out.println("Your Current Position"+position);
     }
 }

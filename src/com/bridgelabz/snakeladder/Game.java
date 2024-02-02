b14df2db6edce633a2a445c1ad279861de91456c
Jawahar-007 <jawaharbhaskaran@gmail.com>
2024-02-03 02:52:58 +0530

    Repeat till the Player reaches the winning position 100. - Note In case the player position moves below 0, then the player restarts from 0

--- src/com/bridgelabz/snakeladder/Game.java
+++ src/com/bridgelabz/snakeladder/Game.java
@@ -4,41 +4,47 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
+    // Roll a Dice
     private static int rolldice(){
         Random r = new Random();
         return r.nextInt(6);
     }
-
+    // Options Generated
     private static String checksfor(){
         String[] arr = {"No play","Ladder","Snake"};
         Random r = new Random();
         int i = r.nextInt(arr.length);
         return arr[i];
     }
-    public static void playgame(){
+
+    public static void playgame() {
         Scanner scanner = new Scanner(System.in);
-        int position =0,diceVal = 0;
-        System.out.println("Initial Position = "+position);
+        int position = 0, diceVal = 0;
+        System.out.println("Initial Position = " + position);
         System.out.println("Enter (roll) to roll dice");
         String ch = scanner.nextLine();
-        if(ch.equalsIgnoreCase("roll")){
+        if (ch.equalsIgnoreCase("roll")) {
             diceVal = rolldice();
-            System.out.println("you rolled out "+diceVal+" times");
+            System.out.println("you rolled out " + diceVal + " times");
         }
-        String Option = checksfor();
-        switch(Option){
-            case "Ladder":
-                position += diceVal;
-                break;
-            case "Snake":
-                position -= diceVal;
-                break;
-            case "No play":
-                position = diceVal;
-                break;
-            default:
-                System.out.println("U got nothing");
+        // To Check the Option and Move Position till 100
+        while (position < 100) {
+            String Option = checksfor();
+            switch (Option) {
+                case "Ladder":
+                    position += diceVal;
+                    break;
+                case "Snake":
+                    position -= diceVal;
+                    if(position<0)
+                        position = 0;
+                    break;
+                case "No play":
+                    break;
+                default:
+                    System.out.println("U got nothing");
+            }
+            System.out.println("Your Current Position" + position);
         }
-        System.out.println("Your Current Position"+position);
     }
 }
